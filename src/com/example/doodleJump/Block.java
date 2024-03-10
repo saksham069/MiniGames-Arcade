3dfa40264b3f51930ec86999d256f87455070fe8
Saksham Arora <sakshamarorax02@gmail.com>
2024-03-10 18:27:47 +0530

    implemented basic blocks deque

--- src/com/example/doodleJump/Block.java
+++ src/com/example/doodleJump/Block.java
@@ -4,8 +4,10 @@ import java.awt.geom.RoundRectangle2D;
 
 class Block {
     RoundRectangle2D collider;
+    double setY;
 
     Block(double x, double y) {
         collider = new RoundRectangle2D.Double(x, y, 100, 10, 20, 20);
+        setY = y;
     }
 }
